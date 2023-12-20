90db5352e026eb9a068c9bd1c98d40cf832b7075
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2023-12-21 01:39:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function makeid(l) {
   // write your code here
+	var text = "";
+  
+  // Define a character list containing uppercase letters, lowercase letters, and digits
+  var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  // Iterate l times to generate each character in the random string
+  for (var i = 0; i < l; i++) {
+    // Append a randomly selected character from char_list to the text string
+    text += char_list.charAt(Math.floor(Math.random() * char_list.length));
+  }
+
+  // Return the generated random string
+  return text;
 }
 
 // Do not change the code below.
